e14d6f7ded4937ecdbe59ac896b851ac33e6fccd
Andrew Kane <andrew@ankane.org>
2026-06-26 19:13:19 -0700

    Removed unneeded plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import buble from "@rollup/plugin-buble";
-import commonjs from "@rollup/plugin-commonjs";
 import pkg from "./package.json" with { type: "json" };
 import resolve from "@rollup/plugin-node-resolve";
 import terser from "@rollup/plugin-terser";
@@ -36,7 +35,6 @@ export default [
     external: external,
     plugins: [
       resolve(),
-      commonjs(),
       buble()
     ]
   },
@@ -52,7 +50,6 @@ export default [
     external: external,
     plugins: [
       resolve(),
-      commonjs(),
       buble(),
       terser()
     ]
